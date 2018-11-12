e6d32ba2150eac960b5564783cdc310329cbafa6
hpj <hpj@hpj.name>
2018-11-12 20:44:24 +0800

    Add files via upload

---
+++ meng.bi/index.php
@@ -0,0 +1,6 @@
+﻿<!--首先说说为什么会有这个子目录，去年在刚建设懵逼网的时候，由于没想到这个网站会有这么多人访问，
+因此把这个demo放在了一个小空间的子目录下，也就是meng.bi/meng.bi，随着流量的增加（虽然是DDoS），
+我不得不放弃小水管的空间，换到了阿里云的独立ECS，但发现依然有人会使用meng.bi/meng.bi，因此增加此
+子目录加以跳转-->
+<div style="display:none"><script src="https://s11.cnzz.com/z_stat.php?id=1261034092&web_id=1261034092" language="JavaScript"></script></div>
+<script language="javascript">window.location='https://meng.bi';</script>
